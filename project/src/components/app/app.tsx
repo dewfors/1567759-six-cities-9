7127675e97a80a2936f4c87ddcc37c8e67f49ec4
Zaur Malakhov <1zaur1@mail.ru>
2022-02-16 15:44:39 +0300

    3-1-1 Добавит маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,38 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../utils/const';
 import PageHome from '../../pages/page-home/page-home';
+import PageLogin from '../../pages/page-login/page-login';
+import PageFavorites from '../../pages/page-favorites/page-favorites';
+import PageOffer from '../../pages/page-offer/page-offer';
+import PageNotFound from '../../pages/page-not-found/page-not-found';
 
 function App(): JSX.Element {
   return (
-    <PageHome />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<PageHome />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<PageLogin />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<PageFavorites />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PageOffer />}
+        />
+        <Route
+          path="*"
+          element={<PageNotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
