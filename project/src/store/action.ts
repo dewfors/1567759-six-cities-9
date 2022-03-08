996c47d879a47d8f2afb1481f3f9c8e4a8b72c53
Zaur Malakhov <1zaur1@mail.ru>
2022-03-08 08:25:32 +0300

    6-1-1 Создаст модули описания действий и редьюсер

---
+++ project/src/store/action.ts
@@ -0,0 +1,5 @@
+import {createAction} from '@reduxjs/toolkit';
+import { Offers } from '../types/offers';
+
+export const setCity = createAction<string>('main/setCity');
+export const setOffers = createAction<Offers>('main/setOffers');
